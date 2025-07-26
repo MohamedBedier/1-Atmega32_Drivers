eee9fd38a32fce052a14c9df4762158a05384e07
Mohamed_Bedier <113776299+MohamedBedier@users.noreply.github.com>
2025-07-26 12:01:36 +0300

    update the PORT driver "more documentation and improve the quality of the code"

--- MohamedBedier_COTS/1-MCAL/2-PORT/PORT_Interface.h
+++ MohamedBedier_COTS/1-MCAL/2-PORT/PORT_Interface.h
@@ -1,18 +1,25 @@
-/*************************************************************/
-/*************************************************************/
-/**                                                         **/
-/**  LayerArchitecture : MCAL                               **/
-/** File Name : PORT_Interface.h                             **/
-/** Auther    : MOHAMED BEDIER MOHAMED                      **/
-/** Verision :  1.00                                        **/
-/**                                                         **/
-/*************************************************************/
-/*************************************************************/
+/*************************************************************
+ *************************************************************
+ *
+ * @layerArchitecture : MCAL
+ * @file :  PORT_Interface.h
+ * @author: MOHAMED BEDIER MOHAMED
+ * @brief:  this file Include function's prototypes, any macros ,and any information for the user
+ * @version:  1.00
+ *
+ *************************************************************
+ *************************************************************/
 
+/**  this is a preprocessor directives commonly used to create "include guards" in header files.
+ *   These guards prevent the contents of a header file from being included multiple times
+ *   during compilation, which can lead to redefinition errors.
+ */
 #ifndef   PORT_INTERFACE_H_
 #define   PORT_INTERFACE_H_
 
-
+/**
+ * @brief : this function is used to initialize port pins
+ */
 void PORT_voidInit(void);
 
 
--- MohamedBedier_COTS/1-MCAL/2-PORT/PORT_Program.c
+++ MohamedBedier_COTS/1-MCAL/2-PORT/PORT_Program.c
@@ -1,32 +1,38 @@
-/*************************************************************/
-/*************************************************************/
-/**                                                         **/
-/**  LayerArchitecture : MCAL                               **/
-/** File Name : PORT_Program.c                              **/
-/** Auther    : MOHAMED BEDIER MOHAMED                      **/
-/** Verision :  1.00                                        **/
-/**                                                         **/
-/*************************************************************/
-/*************************************************************/
+/*************************************************************
+ *************************************************************
+ *
+ * @LayerArchitecture:  MCAL
+ * @file :  PORT_Program.c
+ * @author: MOHAMED BEDIER MOHAMED
+ * @brief:  this file is used to carry memory map addressess for the DIO Registers
+ * @version:  1.00
+ *
+ *************************************************************
+ *************************************************************/
+
 
 #include  "STD_TYPES.h"
-
+#include "BIT_MATH.h"
+#include "defines.h"
  /*  */
 #include  "PORT_Register.h"
 #include  "PORT_Interface.h"
 #include  "PORT_Cfg.h"
 #include  "PORT_Private.h"
 
-
+/**
+ * @brief : this function to initialize port
+ */
 void PORT_voidInit(void)
 {
 
+	/* assign port direction */
 	DDRA = PORTA_DIR ;
 	DDRB = PORTB_DIR ;
 	DDRC = PORTC_DIR ;
 	DDRD = PORTD_DIR ;
 
-
+	/* assign port initial value */
 	PORTA = PORTA_INITIAL_VAL;
 	PORTB = PORTB_INITIAL_VAL;
 	PORTC = PORTC_INITIAL_VAL;
