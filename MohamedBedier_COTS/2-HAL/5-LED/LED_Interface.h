07d19daadb9f0870534e25e481c1865b28d4c0ca
Mohamed_Bedier <113776299+MohamedBedier@users.noreply.github.com>
2025-07-26 12:18:26 +0300

    update the LED driver in HAL  "more documentation and improve the quality of the code " adding README file

--- MohamedBedier_COTS/2-HAL/5-LED/LED_Interface.h
+++ MohamedBedier_COTS/2-HAL/5-LED/LED_Interface.h
@@ -1,13 +1,19 @@
-/*************************************************************/
-/*************************************************************/
-/**                                                         **/
-/**  LayerArchitecture : APP                                **/
-/** File Name : LED_interface.h                             **/
-/** Auther    : MOHAMED BEDIER MOHAMED                      **/
-/** Verision :  1.00                                        **/
-/**                                                         **/
-/*************************************************************/
-/*************************************************************/
+/*************************************************************
+ *************************************************************
+ *
+ * @layerArchitecture : HAL
+ * @file :  LED_interface.h
+ * @author: MOHAMED BEDIER MOHAMED
+ * @brief:  this file Include function's prototypes, any macros ,and any information for the user
+ * @version:  1.00
+ *
+ *************************************************************
+ *************************************************************/
+
+/**  this is a preprocessor directives commonly used to create "include guards" in header files.
+ *   These guards prevent the contents of a header file from being included multiple times
+ *   during compilation, which can lead to redefinition errors.
+ */
 
 #ifndef LED_INTERFACE_H_
 #define LED_INTERFACE_H_
@@ -33,20 +39,36 @@
  					 7-DIO_u8PIN6
  					 8-DIO_u8PIN7
  				                    				  */
-
+/**
+ * @brief: this is the configuration struct for LED
+ */
 
 typedef struct
 {
 	uint8 LED_u8ConnectionType;
 	uint8 LED_u8PORT;
 	uint8 LED_u8PIN;
-}LED_Struct;
+}LED_Config_t;
 
 
-
-uint8  LED_u8LedTurnON(LED_Struct *puFrom_LED_Struct);
-uint8  LED_u8LedTurnOff(LED_Struct *puFrom_LED_Struct);
-uint8 LED_u8ToggleLed(LED_Struct *puFrom_LED_Struct);
+/**
+ * @brief: this function is used to Configure LED on
+ * @param puFrom_LED_Struct : this is a pointer of struct which carrying LED configuration
+ * @return: Local_u8ErrorState : this is a variable used to carry error state value
+ */
+uint8  LED_u8LedTurnON(LED_Config_t *puFrom_LED_Config);
+/**
+ * @brief: this function is used to Configure LED OFF
+ * @param puFrom_LED_Struct : this is a pointer of struct which carrying LED configuration
+ * @return: Local_u8ErrorState : this variable to carry error state value
+ */
+uint8  LED_u8LedTurnOff(LED_Config_t *puFrom_LED_Config);
+/**
+ * @brief: this function is used to Configure LED as toggling
+ * @param puFrom_LED_Struct : this is a pointer of struct which carrying LED configuration
+ * @return: Local_u8ErrorState : this variable to carry error state value
+ */
+uint8 LED_u8LEDToggle(LED_Config_t *puFrom_LED_Config);
 
 
 
